9e4e6e852cab1b56023b082a1c5228a5f7505a71
Your Name <kadry.nukr@ukr.net>
2024-04-09 00:13:21 +0300

    changes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,20 @@
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectFilteredContacts } from '../../redux/contactsSlice';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contactsOps';
 
 const ContactList = () => {
 	const contacts = useSelector(selectFilteredContacts);
 
-	
+	const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+
+   
 
 	return (
 		<ul className={css.list}>
